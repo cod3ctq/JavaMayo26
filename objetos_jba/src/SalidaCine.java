514f40b7e4357b721800f5de48c45a293467d34f
josue-borja17 <josueborja170799@gmail.com>
2026-05-11 14:23:17 -0600

    proyecto objetos jba

---
+++ objetos_jba/src/SalidaCine.java
@@ -0,0 +1,80 @@
+public class SalidaCine {
+
+    String pelicula;
+    double costoBoleto;
+    int cantidadAsiento;
+    String folioAsiento;
+    String horario;
+    String direccionCine;
+
+    public SalidaCine(){}
+
+    public SalidaCine(String pelicula, double costoBoleto, int cantidadAsiento, String folioAsiento, String horario, String direccionCine) {
+        this.pelicula = pelicula;
+        this.costoBoleto = costoBoleto;
+        this.cantidadAsiento = cantidadAsiento;
+        this.folioAsiento = folioAsiento;
+        this.horario = horario;
+        this.direccionCine = direccionCine;
+    }
+
+    public double getCostoBoleto() {
+        return costoBoleto;
+    }
+
+    public void setCostoBoleto(double costoBoleto) {
+        this.costoBoleto = costoBoleto;
+    }
+
+    public String getPelicula() {
+        return pelicula;
+    }
+
+    public void setPelicula(String pelicula) {
+        this.pelicula = pelicula;
+    }
+
+    public int getCantidadAsiento() {
+        return cantidadAsiento;
+    }
+
+    public void setCantidadAsiento(int cantidadAsiento) {
+        this.cantidadAsiento = cantidadAsiento;
+    }
+
+    public String getFolioAsiento() {
+        return folioAsiento;
+    }
+
+    public void setFolioAsiento(String folioAsiento) {
+        this.folioAsiento = folioAsiento;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+    public String getDireccionCine() {
+        return direccionCine;
+    }
+
+    public void setDireccionCine(String direccionCine) {
+        this.direccionCine = direccionCine;
+    }
+
+    @Override
+    public String toString() {
+        return "SalidaCine{" +
+                "pelicula='" + pelicula + '\'' +
+                ", costoBoleto=" + costoBoleto +
+                ", cantidadAsiento=" + cantidadAsiento +
+                ", folioAsiento='" + folioAsiento + '\'' +
+                ", horario='" + horario + '\'' +
+                ", direccionCine='" + direccionCine + '\'' +
+                '}';
+    }
+}
---
+++ objetos_jba/src/Smartphone.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+
+// la herencia permite reutilizar y/o absorber los miembros de una clase en otra.
+// Herencia relacion "ES UN"
+//Permite especializar modelos
+public class Smartphone extends Celular {
+
+    String sistemaOperativo;
+    Camara cam; //atributo compusto
+
+
+    public Smartphone() {
+    }
+
+    public Smartphone(String modelo, String marca, double precio, int mAh, String sistemaOperativo, Camara cam) {
+        super(modelo, marca, precio, mAh);
+        this.sistemaOperativo = sistemaOperativo;
+        this.cam = cam;
+    }
+
+    public String getSistemaOperativo() {
+        return sistemaOperativo;
+    }
+
+    public void setSistemaOperativo(String sistemaOperativo) {
+        this.sistemaOperativo = sistemaOperativo;
+    }
+
+    @Override
+    public String toString() {
+        return "Smartphone{" +
+                "sistemaOperativo='" + sistemaOperativo + '\'' +
+                ", modelo='" + modelo + '\'' +
+                ", marca='" + marca + '\'' +
+                ", precio=" + precio +
+                ", mAh=" + mAh +
+                '}';
+    }
+    // metodo de la forma : Heredado y sobreescrito
+    @Override //<<--- sobreescritura
+    public void mandarMensaje(String numero, String mensaje) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("1 - Mensaje de texto");
+        System.out.println("2 - Mensaje por Whatsapa");
+        System.out.println("3 - Mensaje por Telegram");
+        System.out.println("4 - Mensaje por messenger");
+        System.out.println("5 - Mensaje por Instragram");
+        System.out.println("6 - Mensaje de Voz");
+        System.out.println("Ingresa una opcion");
+        int seleccion = scan.nextInt();
+        switch (seleccion) {
+            case 1:
+                System.out.println("Mandando:[ " + mensaje + " ] por texto");
+                break;
+            case 2:
+                System.out.println("Mandando:[ " + mensaje + " ] por whatsapp");
+                break;
+            case 3:
+                System.out.println("Mandando:[ " + mensaje + " ] por telegram");
+                break;
+            case 4:
+                System.out.println("Mandando:[ " + mensaje + " ] por messenger");
+                break;
+            case 5:
+                System.out.println("Mandando:[ " + mensaje + " ] por instagram");
+                break;
+            case 6:
+                System.out.println("Mandando:[ " + mensaje + " ] por Voz");
+                break;
+        }
+
+
+//    public void hacerLlamada(String numero){
+//        System.out.println("Marcando al numero:> " +numero);
+//    }
+    }
+}
