c118708662d9e613d7070d356ed0cb3028e21687
josue-borja17 <josueborja170799@gmail.com>
2026-05-14 13:07:28 -0600

    proyecto integrador jba

---
+++ proyecto_integrador_jba/src/CajeroBasico.java
@@ -0,0 +1,43 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class CajeroBasico extends Atm implements IOperacionesBasicas{
+    @Override
+    public void cobrarRetirosSinTarjeta() {
+
+    }
+
+    @Override
+    public Object[] retirar(String numTarjeta, double monto, String nip) {
+
+        Cuenta cuenta = this.buscarCuenta(numTarjeta, nip);
+        Object[] datos = new Object[2];
+
+        //Si la cuenta NO existe.....
+        if (! (cuenta!=null) ){
+            System.out.println("La cuenta no existe!. No es posible retirar");
+        }else{
+            //Verificar si me alcanza
+            if (cuenta.getSaldo()<monto){
+                System.out.println("Saldo insuficiente");
+            } else if ( (cuenta.getSaldo() - monto) < Constantes.SALDO_MIN) { //Validar que si retiro, quede por encima del minimo
+                System.out.println("Retiro no disponible. Excede el minimo permitido");
+            }else { //retirar
+                cuenta.setSaldo( cuenta.getSaldo() - monto);
+            Ticket t = new Ticket(this.getDireccion(),
+                    folioOperacion++,
+                    LocalDateTime.now(),
+                    monto, "RETIRO",
+                    "*******"+cuenta.getNumCuenta().substring(8));
+            datos[0] = monto;
+            datos[1] = t;
+            }
+        }
+        return datos;
+    }
+
+    @Override
+    public Ticket pagarServicio(String convenio, String referencia) {
+        return null;
+    }
+}
